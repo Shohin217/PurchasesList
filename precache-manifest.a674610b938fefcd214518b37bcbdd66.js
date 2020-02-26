c41f28439b3598ddd5737b93c384b3a81529c7cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-26 17:54:53 +0000

    Deploy Shohin217/PurchasesList to github.com/Shohin217/PurchasesList.git:gh-pages

---
+++ precache-manifest.a674610b938fefcd214518b37bcbdd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee4026e5b65b36c7bcff0abfb4174a5",
+    "url": "/PurchasesList/index.html"
+  },
+  {
+    "revision": "ee8a69b020fa708abda7",
+    "url": "/PurchasesList/static/css/main.2542e2cf.chunk.css"
+  },
+  {
+    "revision": "d26565cca3c0f602c536",
+    "url": "/PurchasesList/static/js/2.7f779521.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PurchasesList/static/js/2.7f779521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8a69b020fa708abda7",
+    "url": "/PurchasesList/static/js/main.c4ec3250.chunk.js"
+  },
+  {
+    "revision": "39831a1e53976bff7878",
+    "url": "/PurchasesList/static/js/runtime-main.27fe058b.js"
+  }
+]);
